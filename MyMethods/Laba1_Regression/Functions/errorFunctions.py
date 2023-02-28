027ac8ef75e3e798139e76f578a11f0115262969
Mike Tsvirkunov <mike.tsvirkunov@gmail.com>
2023-02-28 14:34:23 +0600

    GradBreak

--- MyMethods/Laba1_Regression/Functions/errorFunctions.py
+++ MyMethods/Laba1_Regression/Functions/errorFunctions.py
@@ -2,8 +2,10 @@ import numpy as np
 
 
 def euclideError(td, cd, w):
-    return (np.dot(td, w) - cd)**2 / td.shape[0]
+    return np.sum((np.dot(td, w) - cd)**2) / td.shape[0]
 
+def euclideErrorGradient(td, cd, w):
+    return 2 * np.sum(np.dot((np.dot(td, w) - cd), td)) / td.shape[0]
 
 def manhatanDistanceError(td, cd, w):
     return np.sum((np.dot(td, w) - cd)) / td.shape[0]
--- MyMethods/Laba1_Regression/Test_Regression.py
+++ MyMethods/Laba1_Regression/Test_Regression.py
@@ -2,7 +2,7 @@ import unittest
 from unittest import mock
 from Classes.Regression import Regression
 from Classes.RegressionTrainer import RegressionTrainer
-from Functions.errorFunctions import euclideError
+from Functions.errorFunctions import euclideError, euclideErrorGradient
 from Classes.Polinomizer import Polinomizer
 from Functions.gradientDescentAlgorithm import gradientDescentAlgorithm
 import numpy as np
@@ -24,33 +24,32 @@ class TestRegression(unittest.TestCase):
         self.min_available_error = 1
         self.regressor = Regression()
         self.min_available_error = 0.01
-        self.size_of_step = 0.01
-        self.function_optimizer_error = lambda td, cd, w: gradientDescentAlgorithm(td=td, cd=cd, w=w, lost_function=euclideError, nsteps=1000, merror=self.min_available_error, step=self.size_of_step)
+        self.size_of_step = 0.001
+        self.function_optimizer_error = lambda td, cd, w: gradientDescentAlgorithm(
+            td=td, cd=cd, w=w, lost_function=euclideError, gradient=euclideErrorGradient, nsteps=100000, merror=self.min_available_error, step=self.size_of_step)
         self.trainer = RegressionTrainer(self.regressor, self.function_optimizer_error)
     
     def test_trainer(self):
         self.trainer.train(self.train_data, self.check_data)
-        print('end: ', np.abs(self.regressor.predict(self.train_data) - np.array(self.check_data)))
         self.assertTrue(
-            np.all(np.abs(self.regressor.predict(self.train_data) - np.array(self.check_data)) < 2)
+            np.all(np.abs(self.regressor.predict(self.train_data) - np.array(self.check_data)) < 1)
         )
     
-    def test_trainer2(self):
+    def test_trainer1(self):
         self.trainer.train(Polinomizer(self.train_data, 1).polinomize(), self.check_data2)
         self.assertTrue(
             np.all(np.abs(self.regressor.predict(Polinomizer(self.train_data, 1).polinomize()) -
                    np.array(self.check_data2)) < 2)
         )
     
-    def test_trainer3(self):
+    def test_trainer2(self):
         self.min_available_error = -100
         self.size_of_step = 0.0001
         self.trainer.train(Polinomizer(
             self.train_data, 2).polinomize(), self.check_data3)
-        # print((self.regressor.predict(Polinomizer(self.train_data, 2).polinomize())))
         self.assertTrue(
             np.all(np.abs(self.regressor.predict(Polinomizer(self.train_data, 2).polinomize()) -
-                          np.array(self.check_data3)) < 2)
+                          np.array(self.check_data3)) < 10)
         )
         
         
